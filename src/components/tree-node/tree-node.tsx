9b6e55adbf30b22410985eb6351f5aa56848537b
dvasilkov <dvasilkov@accesssoftek.com>
2024-01-23 11:15:04 +0300

    tree css

--- src/components/tree-node/tree-node.tsx
+++ src/components/tree-node/tree-node.tsx
@@ -6,6 +6,7 @@ import styles from './tree-node.module.pcss';
 import { Label } from '@components/tree-node/label/label.tsx';
 import { Expander } from '@components/tree-node/expander/expander.tsx';
 import { useTreeSelectContext } from '@reducer/index.ts';
+import classNames from 'classnames';
 
 export const TreeNode: React.FC<{ node: TreeSelectItems }> = ({ node }) => {
   const { state } = useTreeSelectContext();
@@ -14,7 +15,7 @@ export const TreeNode: React.FC<{ node: TreeSelectItems }> = ({ node }) => {
 
   if (state.isSearchMode && (!node.filtered && !node.expanded)) return null;
 
-  return <div className={styles.container}>
+  return <div className={classNames(styles.container, !hasChildren && styles.empty)}>
         <div className={styles.title}>
             {!state.isSearchMode && <Expander node={node} hasChildren={!!hasChildren}/>}
             <Label node={node} hasChildren={!!hasChildren}/>
